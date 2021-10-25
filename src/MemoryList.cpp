1ad14dfa894787a270521c0d82bb7781f63c3408
Zachary Petersen <zacharypetersen1@gmail.com>
2021-10-25 14:10:01 -0700

    Refactor memory block and metadata

--- src/MemoryList.cpp
+++ src/MemoryList.cpp
@@ -1,14 +1,65 @@
 #include "MemoryList.h"
+#include "MemoryUtils.h"
+#include <cassert>
 
 namespace GameMemorySystem
 {
 
-void MemoryBlock::init(bool isFree, size_t size)
+blockPtr createMemoryBlock(U8* pStart, bool isFree, size_t size)
 {
-	// Combine isFree and size into header
-	size_t isFree_AsSizeT = static_cast<size_t>(isFree);
-	size_t shiftedSize = size << 1;
-	m_header = isFree_AsSizeT | shiftedSize;
+	metadata_type* pHeader = reinterpret_cast<metadata_type*>(pStart);
+	writeMetadata(pHeader, isFree, size);
+	U8* end = pStart + size;
+	metadata_type* pFooter = reinterpret_cast<metadata_type*>(end - sizeof(metadata_type));
+	writeMetadata(pFooter, isFree, size);
+	return pHeader;
+}
+
+void writeMetadata(metadata_type* pLocation, bool isFree, size_t size)
+{
+	// Ensure that size does not exceed storage space in header
+	// One bit stores "Free" flag so we have sizeof(metadata_type) * 8 - 1 bits
+	metadata_type maxSize = -1;
+	maxSize = maxSize >> 1;
+	assert(size <= maxSize);
+
+	// Combine isFree and size and write result into desired location
+	metadata_type isFree_bits = static_cast<metadata_type>(isFree);
+	metadata_type size_bits = static_cast<metadata_type>(size) << 1;
+	*pLocation = isFree_bits | size_bits;
+}
+
+bool isFree(const metadata_type* pMetadata)
+{
+	return (*pMetadata & 1) > 0;
+}
+
+size_t getSize(const metadata_type* pMetadata)
+{
+	return *pMetadata >> 1;
+}
+
+blockPtr getPrevBlock(blockPtr pBlock)
+{
+	// Prev block's footer must be directly before this block's header
+	metadata_type* pPrevFooter = pBlock - 1;
+	size_t prevBlockSize = getSize(pPrevFooter);
+
+	// Once we know size of previous block, roll ptr back that many bytes
+	U8* pThisBlockStart = reinterpret_cast<U8*>(pBlock);
+	U8* pPrevBlockStart = pThisBlockStart - prevBlockSize;
+	blockPtr pResult = reinterpret_cast<blockPtr>(pPrevBlockStart);
+	return pResult;
+}
+
+blockPtr getNextBlock(blockPtr pBlock)
+{
+	// Move ptr forward based on how big this block is
+	size_t thisBlockSize = getSize(pBlock);
+	U8* pThisBlockStart = reinterpret_cast<U8*>(pBlock);
+	U8* pNextBlockStart = pThisBlockStart + thisBlockSize;
+	metadata_type* pResult = reinterpret_cast<metadata_type*>(pNextBlockStart);
+	return pResult;
 }
 
 }
--- src/MemoryList.h
+++ src/MemoryList.h
@@ -1,25 +1,33 @@
 #pragma once
+#include "MemoryUtils.h"
 
 namespace GameMemorySystem
 {
 
-// Contains metadata for a memory block
-class MemoryBlock
-{
-private:
-	// Rightmost bit contains isFree flag.
-	// All other bits contain size.
-	size_t m_header = 0;
+// Type used for storing metadata in the header/footer of memory block
+// Rightmost bit contains isFree flag, all other bits contain size.
+typedef size_t metadata_type;
 
-public:
-	// Initializes member data
-	void init(bool isFree, size_t size);
+// a blockPtr points to the header metadata of the block
+typedef metadata_type* blockPtr;
 
-	// Returns true if this block is free
-	inline bool isFree() const { return (m_header & 1) > 0; }
+// Writes metadata that defines the start and end of a memory block.
+// Returns ptr to the header of the block
+blockPtr createMemoryBlock(U8* pStart, bool isFree, size_t size);
 
-	// Returns the size of this block
-	inline size_t getSize() const { return m_header >> 1; }
-};
+// Writes the given metadata into the given location
+void writeMetadata(metadata_type* pLocation, bool isFree, size_t size);
+
+// Returns true if the block is free
+bool isFree(const metadata_type* pMetadata);
+
+// Returns the size of the block
+size_t getSize(const metadata_type* pMetadata);
+
+// Returns header of the next block
+blockPtr getPrevBlock(blockPtr pBlock);
+
+// Returns header of the previous block
+blockPtr getNextBlock(blockPtr pBlock);
 
 }
